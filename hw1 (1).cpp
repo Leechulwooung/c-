46e5d11a5d2d6316ab4006f991af2a8eb970e0bb
Leechulwooung <104194875+Leechulwooung@users.noreply.github.com>
2022-07-28 17:40:29 +0900

    과제 1

---
+++ hw1 (1).cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main(void)
+{
+	printf("건국대학교 20학번 이철웅입니다.\n");
+	printf("저의 나이는 25살 입니다.\n");
+	printf("수원에 거주합니다.\n");
+	printf("컴프터프로그램밍1 수업을 수강합니다.\n");
+	printf("잘 부탁드립니다.\n");
+
+	return 0;
+}
